1537577df4ca11b3360bda886ce259232d138e6b
ZhangZhiQiang <2510406196@qq.com>
2021-05-27 23:57:49 +0800

    echarts init

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,48 +1,91 @@
-// index.js
-// 获取应用实例
-const app = getApp()
+const app = getApp();
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
-  },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
+  onShareAppMessage: function (res) {
+    return {
+      title: 'ECharts 可以在微信小程序中使用啦！',
+      path: '/pages/index/index',
+      success: function () {},
+      fail: function () {}
     }
   },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
+  data: {
+    charts: [{
+      id: 'bar',
+      name: '柱状图'
+    }, {
+      id: 'scatter',
+      name: '散点图'
+    }, {
+      id: 'pie',
+      name: '饼图'
+    }, {
+      id: 'line',
+      name: '折线图'
+    }, {
+      id: 'funnel',
+      name: '漏斗图'
+    }, {
+      id: 'gauge',
+      name: '仪表盘'
+    }, {
+      id: 'k',
+      name: 'K 线图'
+    }, {
+      id: 'radar',
+      name: '雷达图'
+    }, {
+      id: 'heatmap',
+      name: '热力图'
+    }, {
+      id: 'tree',
+      name: '树图'
+    }, {
+      id: 'treemap',
+      name: '矩形树图'
+    }, {
+      id: 'sunburst',
+      name: '旭日图'
+    }, {
+      id: 'map',
+      name: '地图'
+    }, {
+      id: 'graph',
+      name: '关系图'
+    }, {
+      id: 'boxplot',
+      name: '箱型图'
+    }, {
+      id: 'parallel',
+      name: '平行坐标图'
+    }, {
+      id: 'sankey',
+      name: '桑基图'
+    }, {
+      id: 'themeRiver',
+      name: '主题河流图'
+    }],
+    chartsWithoutImg: [{
+      id: 'lazyLoad',
+      name: '延迟加载图表'
+    }, {
+      id: 'multiCharts',
+      name: '一个页面中多个图表'
+    }, {
+      id: 'move',
+      name: '页面不阻塞滚动'
+    }, {
+      id: 'saveCanvas',
+      name: '保存 Canvas 到本地文件'
+    }]
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+  onReady() {
+  },
+
+  open: function (e) {
+    wx.navigateTo({
+      url: '../' + e.target.dataset.chart.id + '/index'
+    });
   }
-})
+});
