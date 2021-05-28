80ea44091594dc269c4086ea70bbd0106f39a5b6
nightmaresss <359643416@qq.com>
2021-05-28 22:13:06 +0800

    fix.

--- app.js
+++ app.js
@@ -39,11 +39,75 @@ App({
       themeListeners.splice(index, 1)
     }
   },
+
+  // 全局数据记录
   globalData: {
     userInfo: null,
     hasLogin: false,
     theme: 'light',
     GRID_DEMO_URL: '/example/index',
-    iconTabbar: '/example/images/icon_tabbar.png'
-  }
+    iconTabbar: '/example/images/icon_tabbar.png',
+    tasks: [{
+        id: 0,
+        name: "待办是您要专注的事",
+        minutes: 1,
+      }, {
+        id: 1,
+        name: "右上角 + 号添加待办",
+        minutes: 10,
+      }, {
+        id: 2,
+        name: "左滑待办编辑或删除",
+        minutes: 25,
+      }, {
+        id: 3,
+        name: "点击开始按钮来专注计时",
+        minutes: 25,
+      }
+    ],
+
+
+    record: [{
+        taskID: 1,
+        recordID: 1,
+        startTime: 123456789,  // format time
+        isFinish: 1,
+        exitTime: 12345,  // ms
+        lastTime: 1234, // ms
+
+      }, {
+
+      }
+    ],
+  },
+
+  getTasks() { // 待完善
+    return this.globalData.tasks
+  },
+
+  getTaskById(id) { // 待完善
+
+    return this.globalData.tasks[id]
+  },
+
+  getRecord() {
+
+  },
+
+  setRecord() {
+
+  },
+
+  deleteRecord() {
+
+  },
+
+  addRecord(record) {
+    this.globalData.push(record)
+  },
+
+
+
+
+
 })
