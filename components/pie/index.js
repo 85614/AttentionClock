2a8cd3a7e418844ec9c05ddfc4fed6e19481a4a9
ZhangZhiQiang <2510406196@qq.com>
2021-05-30 11:48:28 +0800

    Merge dev-zwh into release

--- components/pie/index.js
+++ components/pie/index.js
@@ -85,7 +85,109 @@ Page({
     ec: {
       onInit: initChart
     },
-    today: today.format("yyyy年MM月dd日")
+    today: today.format("yyyy年MM月dd日"),
+    curDate: today,
+    curState: 0, // 0: 日， 1：周， 2：月，3：年
+  },
+
+  // 向前进一个时间段
+  forward() {
+    let cur = this.data.curDate
+    if (this.data.curState === 0) { // 日
+      this.data.curDate.setDate(cur.getDate() + 1)
+    }
+    else if (this.data.curState === 1) { // 周
+      this.data.curDate.setDate(cur.getDate() + 7 - cur.getDay());
+    }
+    else if (this.data.curState === 2) { // 月
+      this.data.curDate.setDate(cur.getFullYear(), cur.getMonth() + 1, 1)
+    }
+    else {  // 年
+      this.data.curDate.setDate(cur.getFullYear() + 1, 1, 1)
+    }
+    this.resetData()
+  },
+
+  // 向后退一个时间段
+  back() {
+    let cur = this.data.curDate
+    if (this.data.curState === 0) { // 日
+      this.data.curDate.setDate(cur.getDate() - 1)
+    }
+    else if (this.data.curState === 1) { // 周
+      this.data.curDate.setDate(cur.getDate() - 7 - cur.getDay());
+    }
+    else if (this.data.curState === 2) { // 月
+      this.data.curDate.setDate(cur.getFullYear(), cur.getMonth() - 1, 1)
+    }
+    else {  // 年
+      this.data.curDate.setDate(cur.getFullYear() - 1, 1, 1)
+    }
+    this.resetData()
+  },
+
+  // 转换状态
+  stateTransition(nxt) {
+    this.data.curState = nxt
+    if (nxt === 1) { // 周
+      this.data.curDate.setDate(cur.getDate() - cur.getDay());
+    }
+    else if (nxt === 2) { // 月
+      this.data.curDate.setDate(cur.getFullYear(), cur.getMonth(), 1)
+    }
+    else if (nxt === 3) {  // 年
+      this.data.curDate.setDate(cur.getFullYear(), 1, 1)
+    }
+    resetData()
+  },
+
+  // 根据新的状态重置数据
+  resetData(){
+    let option = {
+      backgroundColor: "#ffffff",
+      series: [{
+        label: {
+          normal: {
+            fontSize: 14
+          }
+        },
+        type: 'pie',
+        center: ['50%', '50%'],
+        radius: ['20%', '40%'],
+        data: []
+      }]
+    };
+
+    let records    
+    if (this.data.curState === 0) { // 日
+      records = app.getSomeDayRecordstatistics(curDate.getTime())
+      console.log("getting day records ", records)
+    }
+    else if (this.data.curState === 1) { // 周
+      records = app.getSomeWeekRecordstatistics(curDate.getTime())
+      console.log("getting week records ", records)
+    }
+    else if (this.data.curState === 2) { // 月
+      records = app.getSomeMonthRecordstatistics(curDate.getTime())
+      console.log("getting month records ", records)
+    }
+    else { // 年
+      records = app.getSomeYearRecordstatistics(curDate.getTime())
+      console.log("getting year records ", records)
+    }
+
+    const data = []
+    for (let i = 0; i < records.length; ++i) {
+      if (records[i]) {
+        console.log("records[i]", records[i])
+        data.push({
+          name: app.getTaskById(records[i].taskID).name,
+          value: records[i].total_time
+        })
+      }
+    }
+
+    _chart.setOption(option)
   },
 
   test(){
